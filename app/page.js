90f1fd2fa9ea77da0e2022b21362845927cbbac3
mildred522 <1037950115@qq.com>
2025-03-04 21:08:03 +0800

    '2'

--- app/page.js
+++ app/page.js
@@ -33,43 +33,45 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            这是我的博客{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              MILDRED
-            </code>
-            .
+        <ol className="text-lg text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-10">
+            这是{"MILDRED"}的博客。
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-background text-foreground gap-2 hover:bg-[#ffffff] dark:hover:bg-[#ffffff] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://github.com/mildred522"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/picgithub.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
             />
-            Deploy now
+            访问github主页
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-background text-foreground gap-2 hover:bg-[#ffffff] dark:hover:bg-[#ffffff] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://ycna8o9c6bcq.feishu.cn/wiki/VKP3w7uTJij2vfk16MBcAeLJnpc?from=from_copylink"
             target="_blank"
             rel="noopener noreferrer"
           >
+            <Image
+              className="dark:invert"
+              src="/picfeishu.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
             查看飞书文档
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-background text-foreground gap-2 hover:bg-[#ffffff] dark:hover:bg-[#ffffff] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
